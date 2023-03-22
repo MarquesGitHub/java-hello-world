dbd8b89847a74fdc50a8b2f83239b518641d50c7
MarquesGitHub <jose.marques.mailbox@gmail.com>
2023-03-22 20:28:02 +0000

    Alteração do texto a mostrar

--- src/pt/serro/test/Main.java
+++ src/pt/serro/test/Main.java
@@ -2,6 +2,7 @@ package pt.serro.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Hi all and Hello World!");
     }
 }
